89c87316680b2825e06df7615236f954ff1d1bf6
Henrik Kälvegren <henrik.kalvegren@hotmail.com>
2018-03-03 12:57:01 +0100

    Connected html chat with server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 
 // Connection URL
-const url = 'mongodb://localhost/mongo';
+const url = 'mongodb://127.0.0.1/socketchat';
 
 const dbName = 'SocketChat';
 
@@ -16,18 +16,22 @@ mongo.connect(url, function(err, db){
 
   // Connect to Socket.io
   client.on('connection', function(socket){
+      console.log("onConnection");
       // Create a collection called 'chats' in mongodb
-      let chat = db.collection('chats');
+      let chat = db.db('socketchat').collection('chats');
 
       // Create function to send status to client from Server
       sendStatus = function(s){
+        console.log('statusing');
         socket.emit('status', s); // pass from server to client
       }
 
-      // Get chats from mongo collection
+      // Query chats from mongo collection
       chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
         assert.equal(null, err);
-        // Emit the messages
+
+        // Send query results to socket
+        console.log('outputting');
         socket.emit('output', res);
 
 
